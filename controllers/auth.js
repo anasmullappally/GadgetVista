e24b4fa1cd5d222b9c1a80a35347a262ac23e7ea
anasmullappally <mohammedanas362@gmail.com>
2023-11-26 22:51:30 +0530

    signup api

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,44 +6,54 @@ import {
   HTTP_STATUS_CREATED,
 } from "../utils/status-codes.js";
 import { userValidationSchema } from "../validation/auth-validation.js";
+import bcrypt from "bcrypt";
 
 export const signup = async (req, res, next) => {
   try {
+    //validating body data using joi schema
     const { value, error } = userValidationSchema.validate(req.body);
     if (error) {
       return res
         .status(HTTP_BAD_REQUEST)
         .json({ success: false, message: error.details[0].message });
     }
-
+    // destructuring
     const { firstName, lastName, email, phoneNumber, password, dateOfBirth } =
       value;
 
+    //checking email already exist or not
     const emailExist = await User.exists({ email: email?.toLowerCase() });
-    if (emailExist)
+    if (emailExist) {
       return res
         .status(HTTP_BAD_REQUEST)
         .json({ success: false, message: signupMessages["email-exist"] });
-
+    }
+    // checking phone number already exists or not
     const phoneExist = await User.exists({ phoneNumber });
-    if (phoneExist)
+    if (phoneExist) {
       return res
         .status(HTTP_BAD_REQUEST)
         .json({ success: false, message: signupMessages["phone-exist"] });
+    }
+
+    //encrypt user password
+    const hashPassword = await bcrypt.hash(password, 10);
 
     const user = new User({
-      firstName,
-      lastName,
-      email,
-      password,
+      firstName: firstName?.toLowerCase(),
+      lastName: lastName?.toLowerCase(),
+      email: email?.toLowerCase(),
+      password: hashPassword,
       phoneNumber,
       dateOfBirth,
     });
+
+    //save data
     await user.save();
 
     return res
       .status(HTTP_STATUS_CREATED)
-      .json({ message: "success", success: true });
+      .json({ message: signupMessages["success"], success: true });
   } catch (error) {
     logger.error({ label: "signup", message: error.message });
     next(error);
--- utils/message-store/auth.js
+++ utils/message-store/auth.js
@@ -2,4 +2,5 @@ export const signupMessages = {
   "email-exist": "The email is already associated with an existing account.",
   "phone-exist":
     "The phone number is already associated with an existing account.",
+  success: "Account created successfully",
 };
